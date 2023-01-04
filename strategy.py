9eca543c0f3d291748e1f65b7b4f019ff38aa760
Jay Coskey <jay.coskey@gmail.com>
2023-01-03 20:50:54 -0800

    Remove test code that was moved to strategy_test.py

--- strategy.py
+++ strategy.py
@@ -184,38 +184,3 @@ class Strategy:
             for b,r in result.items():
                 print(f'Greedy roll for {b.name:<10} is {r}')
         return result
-
-
-if __name__ == '__main__':
-    def assert_reroll_for_box(roll, box, expected):
-        observed = Strategy.goals_to_rerolls(roll, [box])[box]
-        if observed != expected:
-            print(f'Expected {expected}, but observed {observed}')
-        assert(observed == expected)
-
-    assert_reroll_for_box([1,1,2,3,3], Box.ACES,   [2,3,4])
-    assert_reroll_for_box([1,1,2,3,3], Box.TWOS,   [0,1,3,4])
-    assert_reroll_for_box([1,1,2,3,3], Box.THREES, [0,1,2])
-    assert_reroll_for_box([4,1,4,3,4], Box.FOURS,  [1,3])
-    assert_reroll_for_box([5,5,5,5,5], Box.FIVES,  [])
-    assert_reroll_for_box([1,2,3,4,5], Box.SIXES,  [0,1,2,3,4])
-
-    assert_reroll_for_box([1,2,3,4,5], Box.KIND3,  [0,1,2,3])
-
-    assert_reroll_for_box([1,2,3,4,5], Box.KIND4,  [0,1,2,3])
-
-    assert_reroll_for_box([1,1,1,1,1], Box.FULL_HOUSE, [3, 4])
-    assert_reroll_for_box([1,1,2,2,3], Box.FULL_HOUSE, [4])
-    assert_reroll_for_box([1,1,1,2,3], Box.FULL_HOUSE, [3])
-
-    assert_reroll_for_box([1,2,3,6,6], Box.STRAIGHT_SMALL, [3,4])
-    assert_reroll_for_box([1,2,4,6,6], Box.STRAIGHT_SMALL, [3,4])
-    assert_reroll_for_box([2,3,3,3,4], Box.STRAIGHT_SMALL, [2,3])
-
-    assert_reroll_for_box([1,2,3,4,5], Box.STRAIGHT_LARGE, [])
-    assert_reroll_for_box([1,2,3,4,6], Box.STRAIGHT_LARGE, [4])
-    assert_reroll_for_box([1,2,3,4,6], Box.STRAIGHT_LARGE, [4])
-    assert_reroll_for_box([1,2,4,5,6], Box.STRAIGHT_LARGE, [0])
-    assert_reroll_for_box([1,2,3,4,4], Box.STRAIGHT_LARGE, [4])
-
-    assert_reroll_for_box([1,2,3,4,5], Box.CHANCE, [0,1,2])
